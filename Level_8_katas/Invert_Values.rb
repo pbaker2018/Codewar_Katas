6a2f201f3064ba322c97769f1ab488767841947b
Pauline <pbaker@hotmail.co.uk>
2019-02-04 09:47:57 +0000

    invert values - level 8

---
+++ Level_8_katas/Invert_Values.rb
@@ -0,0 +1,14 @@
+# QUESTION
+# Given a set of numbers, return the additive inverse of each. Each positive becomes negatives, and the negatives become positives.
+#
+# invert([1,2,3,4,5]) == [-1,-2,-3,-4,-5]
+# invert([1,-2,3,-4,5]) == [-1,2,-3,4,-5]
+# invert([]) == []
+# You can assume that all values are integers.
+#
+#
+# SOLUTION
+
+def invert(array)
+  array.map{|e|e * -1}
+end
