b3434075934424a0f76dcf30d5cae82ca03b31cc
Pauline <pbaker@hotmail.co.uk>
2019-02-04 09:32:20 +0000

    double char - level 8

---
+++ Level_8_katas/Double_Char.rb
@@ -0,0 +1,17 @@
+# QUESTION
+# Given a string, you have to return a string in which each character (case-sensitive) is repeated once.
+#
+# double_char("String") ==> "SSttrriinngg"
+#
+# double_char("Hello World") ==> "HHeelllloo  WWoorrlldd"
+#
+# double_char("1234!_ ") ==> "11223344!!__  "
+# Good Luck!
+
+
+
+# SOLUTION
+
+def double_char(str)
+ str.chars.map{|e|e *2}.join
+end
