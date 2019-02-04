3dc031bff00b2450bb3e7a11f41bfcf0090173e5
Pauline <pbaker@hotmail.co.uk>
2019-02-04 15:11:35 +0000

    find the next perfect square - level 7

---
+++ Level_7_katas/find_the_next_perfect_square.rb
@@ -0,0 +1,27 @@
+# QUESTION
+# You might know some pretty large perfect squares. But what about the NEXT one?
+#
+# Complete the findNextSquare method that finds the next integral perfect square
+# after the one passed as a parameter. Recall that an integral perfect square is
+# an integer n such that sqrt(n) is also an integer.
+#
+# If the parameter is itself not a perfect square, then -1 should be returned.
+# You may assume the parameter is positive.
+#
+# Examples:
+#
+# findNextSquare(121) --> returns 144
+# findNextSquare(625) --> returns 676
+# findNextSquare(114) --> returns -1 since 114 is not a perfect
+#
+# SOLUTION
+
+def find_next_square(sq)
+  if Math.sqrt(sq) % 1 != 0
+    -1
+  else
+    array = []
+    array << [*sq+1..sq+1000000]
+    array[0].detect{|e|Math.sqrt(e) % 1 == 0}
+  end
+end
