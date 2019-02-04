12f01540643e88d73079a95021373a19ec823bc4
Pauline <pbaker@hotmail.co.uk>
2019-02-04 11:34:26 +0000

    does my number look big in this? - level 6

---
+++ Level_6_katas/does_my_number_look_big_in_this.rb
@@ -0,0 +1,31 @@
+# QUESTION
+# A Narcissistic Number is a number which is the sum of its own digits,
+# each raised to the power of the number of digits in a given base.
+# In this Kata, we will restrict ourselves to decimal (base 10).
+#
+# For example, take 153 (3 digits):
+#
+#     1^3 + 5^3 + 3^3 = 1 + 125 + 27 = 153
+#
+# Another example, take 1634 (4 digits):
+#
+#     1^4 + 6^4 + 3^4 + 4^4 = 1 + 1296 + 81 + 256 = 1634
+#
+# The Challenge:
+#
+# Your code must return true or false depending upon whether the given number is
+# a Narcissistic number in base 10.
+#
+# Error checking for text strings or other invalid inputs is not required, only
+# valid integers will be passed into the function.
+#
+#
+# SOLUTION
+
+def narcissistic?( value )
+  narcissistic = value.digits.reverse.reduce(0){
+    |sum, num| sum += num ** value.digits.length
+  }
+  return true if narcissistic == value
+  return false if narcissistic != value
+end
