491950fa56321142b6a57153f6926fdd080aa7bd
Pauline <pbaker@hotmail.co.uk>
2019-02-06 15:11:07 +0000

    shortest word - level 7 - with question commented out

--- Level_7_katas/shortest_word.rb
+++ Level_7_katas/shortest_word.rb
@@ -1,9 +1,9 @@
-QUESTION
-Simple, given a string of words, return the length of the shortest word(s).
-String will never be empty and you do not need to account for different data types.
-
-
-SOLUTION
+# QUESTION
+# Simple, given a string of words, return the length of the shortest word(s).
+# String will never be empty and you do not need to account for different data types.
+#
+#
+# SOLUTION
 
 def find_short(s)
   s.split(' ').map{|e|e.length}.min
