c5c740830d48a8535a8439dc309118651c8ca5bc
Pauline <pbaker@hotmail.co.uk>
2019-02-06 14:36:53 +0000

    find the parity outlier - level 6

---
+++ Level_6_katas/find_the_parity_outlier.rb
@@ -0,0 +1,25 @@
+# QUESTION
+# You are given an array (which will have a length of at least 3, but could be very large)
+# containing integers. The array is either entirely comprised of odd integers or
+# entirely comprised of even integers except for a single integer N.
+# Write a method that takes the array as an argument and returns this "outlier" N.
+#
+# Examples
+# [2, 4, 0, 100, 4, 11, 2602, 36]
+# Should return: 11 (the only odd number)
+#
+# [160, 3, 1719, 19, 11, 13, -21]
+# Should return: 160 (the only even number)
+#
+#
+# SOLUTION
+
+def find_outlier(array)
+  even = array.count{|e|e.even?}
+  odd = array.count{|e|e.odd?}
+  if even > odd
+    array.find{|e|e.odd?}
+  else
+    array.find{|e|e.even?}
+  end
+end
