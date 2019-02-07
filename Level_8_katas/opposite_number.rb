d18a922ccef959736d59714d440cc3f71950458e
Pauline <pbaker@hotmail.co.uk>
2019-02-07 13:35:01 +0000

    opposite number - level 8

---
+++ Level_8_katas/opposite_number.rb
@@ -0,0 +1,17 @@
+# QUESTION
+# Very simple, given a number, find its opposite.
+#
+# Examples:
+#
+# 1: -1
+# 14: -14
+# -34: 34
+#
+# But can you do it in 1 line of code and without any conditionals?
+#
+#
+# SOLUTION
+
+def opposite(num)
+  - num
+end
