75350b30da5e6489edf8532bdeb994003388cc9b
Pauline <pbaker@hotmail.co.uk>
2019-02-07 11:11:32 +0000

    convert boolean values to strings 'yes' or 'no' - level 8

---
+++ Level_8_katas/convert_boolean_values_to_strings_yes_or_no.rb
@@ -0,0 +1,10 @@
+# QUESTION
+# Complete the method that takes a boolean value and returns
+# a "Yes" string for true, or a "No" string for false.
+#
+#
+# SOLUTION
+
+def bool_to_word(bool)
+  bool ? "Yes" : "No"
+end
