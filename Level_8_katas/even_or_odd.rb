05e82e0644427e8c07ff7d126a8a8471a1c5f34a
Pauline <pbaker@hotmail.co.uk>
2019-02-07 13:56:14 +0000

    even or odd - level 8

---
+++ Level_8_katas/even_or_odd.rb
@@ -0,0 +1,10 @@
+# QUESTION
+# Create a function (or write a script in Shell) that takes an integer as an
+# argument and returns "Even" for even numbers or "Odd" for odd numbers.
+#
+#
+# SOLUTION
+
+def even_or_odd(number)
+  number % 2 == 0 ? "Even" : "Odd"
+end
